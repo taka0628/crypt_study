17ee705abc193313a2146e17592eb88df1aa9a30
taka0628 <takatsun15@gmail.com>
2020-11-11 15:44:15 +0900

    arduino課題完了

---
+++ sensor_net/4/4-3/sketch_nov11c/sketch_nov11c.ino
@@ -0,0 +1,13 @@
+const int pinrotary = A0;
+const int pinled = 3;
+void setup() {
+ Serial.begin(9600); // シリアルモニタと 9600bps で通信するように設定
+pinMode(pinrotary, INPUT); // Rotary Angle Sensor を接続したポートを入力に設定
+pinMode(pinled, 3); // LED を接続したポートを出力に設定
+}
+void loop() {
+ int value = analogRead(pinrotary); // Rotary Angle Sensor から値を読み取り
+ Serial.println(value); // センサーから読み取った値をシリアルモニタにログ表示
+ analogWrite(pinled, 256 - value/4); // センサーから読み取った値の 1/4 を LED の明るさに設定
+ delay(100); // 100 ミリ秒待機
+}
