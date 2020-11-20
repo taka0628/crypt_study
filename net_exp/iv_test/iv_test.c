2ea5f06ac31ca6a42b19eefa34166cb72804c04f
taka0628 <takatsun15@gmail.com>
2020-11-20 21:38:43 +0900

    鍵生成まで理解 DHの勉強中

---
+++ net_exp/iv_test/iv_test.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+#include <openssl/dh.h>
+
+void encrypt(char *out, const char *in, const uint8_t *key, const uint8_t *iv, int size)
+{
+    int out_len, in_len = size;
+    EVP_CIPHER_CTX *ctx;
+
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+    {
+        printf("error EVP_CIPHER_CTX_new\n");
+        return;
+    }
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, key, iv))
+    {
+        printf("error EVP_EncryptInit_ex\n");
+        return;
+    }
+
+    if (1 != EVP_EncryptUpdate(ctx, (unsigned char *)out, &out_len, (unsigned char *)in, in_len))
+    {
+        printf("error EVP_EncryptUpdate\n");
+        return;
+    }
+    if (out_len != in_len)
+    {
+        printf("error encrypt out_len(%d) != in_len(%d)\n", out_len, in_len);
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+}
+
+void decrypt(char *out, const char *in, const uint8_t *key, const uint8_t *iv, int size)
+{
+    int out_len, in_len = size;
+    EVP_CIPHER_CTX *ctx;
+
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+    {
+        printf("error EVP_CIPHER_CTX_new\n");
+        return;
+    }
+
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, key, iv))
+    {
+        printf("error EVP_DecryptInit_ex\n");
+        return;
+    }
+
+    if (1 != EVP_DecryptUpdate(ctx, out, &out_len, in, in_len))
+    {
+        printf("error EVP_DecryptUpdate\n");
+        return;
+    }
+    if (out_len != in_len)
+    {
+        printf("error decrypt out_len(%d) != in_len(%d)\n", out_len, in_len);
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+}
+
+void print(char *header, uint8_t *buf, int size)
+{
+    int i;
+    printf("%s", header);
+    for (i = 0; i < size; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    time_t tm;
+    time(&tm);
+    char key[16] = {'\0'};
+    char iv[16] = {'\0'};
+    RAND_bytes(key, 128 / 8);
+    print("key: ", key, sizeof(key));
+    RAND_bytes(iv, 128 / 8);
+    print("key: ", iv, sizeof(iv));
+
+    EVP_PKEY_CTX *ctx;
+    size_t key_len;
+    EVP_PKEY *shared_key;
+    DH     *a;
+	DH     *b = NULL;
+    ctx = EVP_PKEY_CTX_new(shared_key, NULL);
+    EVP_PKEY_derive_init(ctx);
+
+}
