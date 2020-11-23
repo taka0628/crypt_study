5a1c28b7ac31f938cc1c1eb8bc1e323f6f0d5654
taka0628 <takatsun15@gmail.com>
2020-11-23 16:08:15 +0900

    DHの鍵生成まで完了

--- net_exp/iv_test/iv_test.c
+++ net_exp/iv_test/iv_test.c
@@ -81,6 +81,39 @@ void print(char *header, uint8_t *buf, int size)
     printf("\n");
 }
 
+void create_DH_key(DH *a, int keyLen, int code)
+{
+    puts("DH start");
+    DH_check(a, &code);
+    if (code & DH_CHECK_P_NOT_PRIME)
+    {
+        fprintf(stderr, "p value is not prime\n");
+    }
+    if (code & DH_CHECK_P_NOT_SAFE_PRIME)
+    {
+        fprintf(stderr, "p value is not a safe prime\n");
+    }
+    if (code & DH_UNABLE_TO_CHECK_GENERATOR)
+    {
+        fprintf(stderr, "unable to check the generator value\n");
+    }
+    if (code & DH_NOT_SUITABLE_GENERATOR)
+    {
+        fprintf(stderr, "the g value is not a generator\n");
+    }
+
+    printf("\n");
+    DHparams_print_fp(stdout, a);
+    printf("\n");
+
+    DH_generate_key(a);
+
+    const BIGNUM *a_pub_key;
+    const BIGNUM *a_priv_key;
+    DH_get0_key(a, &a_pub_key, &a_priv_key);
+    printf("pub_key: %s\npriv_key: %s\n", BN_bn2hex(a_pub_key), BN_bn2hex(a_priv_key));
+}
+
 int main()
 {
     time_t tm;
@@ -95,9 +128,9 @@ int main()
     EVP_PKEY_CTX *ctx;
     size_t key_len;
     EVP_PKEY *shared_key;
-    DH     *a;
-	DH     *b = NULL;
-    ctx = EVP_PKEY_CTX_new(shared_key, NULL);
-    EVP_PKEY_derive_init(ctx);
-
+    DH *a = DH_new();
+    int keyLen = 64;
+    int code = 0;
+    DH_generate_parameters_ex(a, keyLen, DH_GENERATOR_5, NULL);
+    create_DH_key(a, key_len, code);
 }
