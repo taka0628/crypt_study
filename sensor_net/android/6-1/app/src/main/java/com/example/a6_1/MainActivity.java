e9d95407ef13d1f6778504bd60395909cf35ca24
taka0628 <takatsun15@gmail.com>
2020-12-09 13:25:52 +0900

    android 6-2 調整

--- sensor_net/android/6-1/app/src/main/java/com/example/a6_1/MainActivity.java
+++ sensor_net/android/6-1/app/src/main/java/com/example/a6_1/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String CHARACTERISTIC_CONFIG_UUID = "00002902-0000-1000-8000-00805f9b34fb";
 
     // ペリフェラルと接続しない: 0, ペリフェラルと接続する: 1
-    private int flag_connect = 1;
+    private int flag_connect = 0;
 
     // グラフ表示用の変数
     private int num;
@@ -105,8 +105,10 @@ public class MainActivity extends AppCompatActivity {
         colors[1] = Color.rgb(0x00, 0xFF, 0x00); // 緑
         //       colors[2] = Color.rgb(0x00, 0x00, 0xFF); // 青
 
-        max = 1000;
-        min = -100;
+//        max = 1000;
+//        min = -100;
+        max = 100;
+        min = -500;
 
         // グラフViewを初期化する
         initChart();
@@ -255,6 +257,8 @@ public class MainActivity extends AppCompatActivity {
 
                 String text = "Found: " + result.getDevice().getName() + ", " + values[1];
                 Log.d("blelog", text);
+
+//                System.out.printf("connect power: %f\n", result.getRssi());
 
                 if(flag_connect == 1){
                     result.getDevice().connectGatt(getApplicationContext(), false, mGattCallback);
