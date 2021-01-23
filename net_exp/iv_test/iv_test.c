d61b773868aa0a3a9b3c0c2983693a50da54c443
taka0628 <takatsun15@gmail.com>
2021-01-23 21:47:03 +0900

    RSA暗号の導入

--- net_exp/iv_test/iv_test.c
+++ net_exp/iv_test/iv_test.c
@@ -10,9 +10,19 @@
 #include <openssl/aes.h>
 #include <openssl/sha.h>
 #include <openssl/rand.h>
+#include <openssl/rsa.h>
 #include <openssl/dh.h>
+#include <openssl/engine.h>
 
-#define ERROR (printf("[ERROR]\n%s: %d\n", __func__, __LINE__))
+#define ERROR(comment)                                                \
+    printf("[ERROR]\n\t%s: %d\n\t%s\n", __func__, __LINE__, comment); \
+    exit(0);
+
+typedef struct
+{
+    char *data;
+    unsigned int size;
+} dynamic_mem_t;
 
 void encrypt(char *out, const char *in, const uint8_t *key, const uint8_t *iv, int size)
 {
@@ -115,8 +125,8 @@ bool DH_first_set(DH *a, int keyLen, int code)
     DH_check(a, &code);
     if (DH_code_check(code) == false)
     {
-        ERROR;
-        puts("DH_key is not safe");
+        ERROR("DH_key is not safe");
+        // puts("DH_key is not safe");
         exit(1);
     }
     printf("\n");
@@ -133,6 +143,96 @@ void create_DH_key(DH *a, const BIGNUM *pub_key, const BIGNUM *priv_key)
     printf("pub_key: %s\npriv_key: %s\n", BN_bn2hex(pub_key), BN_bn2hex(priv_key));
 }
 
+// RSA暗号の鍵を生成
+bool rsa_key_set(const unsigned int key_size, EVP_PKEY *pkey)
+{
+    printf("RSA-%dで鍵を生成します\n", key_size);
+    int rc = 1;
+    // RSAの設定
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    if (!ctx)
+    {
+        ERROR("EVP_PKEY_CTX");
+        return false;
+    }
+    if (EVP_PKEY_keygen_init(ctx) <= 0)
+    {
+        ERROR("EVP_PKEY_keygen_init");
+        return false;
+    }
+    if ((rc = EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, key_size)) <= 0)
+    {
+        ERROR("EVP_PKEY_CTX");
+        return false;
+    }
+    //  鍵生成
+    if ((rc = EVP_PKEY_keygen(ctx, &pkey)) <= 0)
+    {
+        ERROR("key_gen");
+        return false;
+    }
+    EVP_PKEY_CTX_free(ctx);
+
+    return true;
+}
+
+// RSA暗号の公開鍵を取得
+void get_rsa_pubkey(EVP_PKEY *pkey, BIGNUM **pubn, BIGNUM **pube)
+{
+    RSA *rsa = RSA_new();
+    rsa = EVP_PKEY_get1_RSA(pkey);
+    RSA_get0_key(rsa, (const BIGNUM **)pubn, (const BIGNUM **)pube, NULL);
+    printf("RSA KEY\npn: %s\npe %s\n", BN_bn2hex(*pubn), BN_bn2hex(*pube));
+}
+
+void enchiper_RSA(EVP_PKEY *pkey, BIGNUM *pubn, BIGNUM *pube, dynamic_mem_t in, dynamic_mem_t *result)
+{
+    printf("平文: %s\n", in.data);
+
+    RSA *rsa = RSA_new();
+    RSA_set0_key(rsa, pubn, pube, NULL);
+    EVP_PKEY_set1_RSA(pkey, rsa);
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(pkey, NULL);
+    if (!ctx)
+    {
+        ERROR("EVP_PKEY_CTX_new");
+    }
+    if (EVP_PKEY_encrypt_init(ctx) <= 0)
+    {
+        ERROR("EVP_PKEY_encrypt_init");
+    }
+    if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_PADDING) <= 0)
+    {
+        ERROR("EVP_PKEY_CTX_set_rsa_padding");
+    }
+    int out_len = 0;
+    if (EVP_PKEY_encrypt(ctx, NULL, (size_t *)&out_len, (unsigned char *)in.data, in.size) <= 0)
+    {
+        ERROR("first EVP_PKEY_encrypt");
+    }
+    printf("outlen: %d\n", out_len);
+    unsigned char *out = malloc(out_len);
+    if(!out){
+        ERROR("out");
+    }
+    // if (EVP_PKEY_encrypt(ctx, out, &out_len, (const unsigned char *)in.data, in.size) <= 0)
+    // {
+    //     ERROR("EVP_PKEY_encrypt out");
+    // }
+    RSA_public_encrypt(RSA_size(rsa), (const unsigned char *)in.data, out, rsa, RSA_PKCS1_PADDING);
+    print("暗号化されたデータ: ", out, out_len);
+    result->size = (unsigned int)out_len;
+    puts("malloc");
+    printf("size: %d\n", result->size);
+    puts("cpy");
+    strncpy(result->data, out, result->size);
+    RSA_free(rsa);
+    EVP_PKEY_free(pkey);
+    free(out);
+    puts("return");
+    return;
+}
+
 // bool get_DH_key(DH *dh, )
 
 int main()
@@ -146,24 +246,32 @@ int main()
     RAND_bytes(iv, 128 / 8);
     print("iv:\t", iv, sizeof(iv));
 
-    DH *a = DH_new();
-    const BIGNUM *a_pub_key = NULL;
-    const BIGNUM *a_priv_key = NULL;
-    int keyLen = 64;
-    int code = 0;
-    DH_generate_parameters_ex(a, keyLen, DH_GENERATOR_5, NULL);
-    DH_first_set(a, keyLen, code);
+    // RSA_generate_key_ex(rsa, 2048, bn, NULL);
+    // // RSA_get0_key(rsa, NULL, &pubkey, &privkey);
+    // EVP_PKEY_assign_RSA(p_key, rsa);
+    // unsigned char str_pubkey[4096];
+    // unsigned char str_privbkey[4096];
+    // RSA_get0_key(rsa, &pubkey, )
+    // // printf("pkey: %x, okey: %x\n", BN_bn2hex(p_key), BN_bn2hex(bn));
+    // printf("pkey: %s, okey: %s\n", str_privbkey, str_pubkey);
 
-    puts("A");
-    create_DH_key(a, a_pub_key, a_priv_key);
+    EVP_PKEY *pkey = EVP_PKEY_new();
+    rsa_key_set(2048, pkey);
 
-    puts("B");
-    DH *b = DH_new();
-    DH_generate_parameters_ex(b, keyLen, DH_GENERATOR_5, NULL);
-    DH_first_set(b, keyLen, code);
-    const BIGNUM *b_pub_key = NULL;
-    const BIGNUM *b_priv_key = NULL;
-    create_DH_key(b, b_pub_key, b_priv_key);
+    BIGNUM *pubn = BN_new(), *pube = BN_new();
+    get_rsa_pubkey(pkey, &pubn, &pube);
+    printf("鍵確認\npn: %s\npe: %s\n", BN_bn2hex(pubn), BN_bn2hex(pube));
+
+    dynamic_mem_t in;
+    in.size = 1024;
+    in.data = malloc(in.size);
+    dynamic_mem_t out;
+    out.data = (char *)malloc(1024);
+    strncpy(in.data, "RSAテスト", in.size);
+    EVP_PKEY *inpkey = EVP_PKEY_new();
+    enchiper_RSA(inpkey, pubn, pube, in, &out);
+
+    print("main 受け取り: ", out.data, out.size);
 
     return 0;
 }
